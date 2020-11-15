4bb611f6c1558de477cf6e2c52abf30fffd64567
James Anderson <james.anderson@pro-serv.co.uk>
2020-11-15 19:47:42 +0000

    First Commit

---
+++ send_email_gmail/readme.md
@@ -0,0 +1,9 @@
+### Send Email Using SMTP with Gmail account
+
+Simply update the gmail_user and gmail_password variables with your gmail account details
+
+#### Note
+
+"Less Secure Apps" must be enabled on the gmail account.
+
+https://support.google.com/accounts/answer/6010255?hl=en
---
+++ send_email_gmail/send_email.py
@@ -0,0 +1,26 @@
+import smtplib
+
+gmail_user = 'me@gmail.com
+gmail_password = '12345678'
+
+sent_from = gmail_user
+
+def send_email(to, subject, body):
+    email_text = """\
+    From: %s
+    To: %s
+    Subject: %s
+
+    %s
+    """ % (sent_from, ", ".join(to), subject, body)
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_password)
+        server.sendmail(sent_from, to, email_text)
+        server.close()
+
+        print ('Email sent!')
+    except:
+        print ('Something went wrong...')
