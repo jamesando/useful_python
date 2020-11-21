e1ab67be65290eb36d806b1dcf14ae98c818a300
James Anderson <james.anderson@pro-serv.co.uk>
2020-11-21 22:17:35 +0000

    Domain Checker Started

---
+++ domain_checker/domain_checker.py
@@ -0,0 +1,23 @@
+import os
+import requests
+from requests import get
+import csv
+from bs4 import BeautifulSoup
+
+domain_list = []
+
+# Read in the domains from the txt file into a list
+with open("domains.txt", "r") as domains:
+    for row in domains:
+        domain_list.append(row.strip('\n'))
+
+# Prepare the requests
+url = "https://uk.godaddy.com/domainsearch/find?checkAvail=1&domainToCheck="
+
+# Loop through the list and return the web page
+for domain in domain_list:
+    request_url = url + domain
+    response = get(request_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    mydivs = soup.findAll("div", {"class": "exact-header"})
+    print(mydivs)
